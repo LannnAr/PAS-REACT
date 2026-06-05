5168aa640acc6e71b78a9e69a0c27e36ed60a19c
LannnAr <fadlan.arofiq@gmail.com>
2026-06-05 14:12:35 +0700

    new update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ function Footer() {
     <footer className="app-footer">
       <p>React Hooks assignment: useState, useEffect, useContext, useRef, Fetch API.</p>
       <p>Data user diambil dari JSONPlaceholder.</p>
+      <p>© Fadlan Ar 2026 React User Dashboard. All rights reserved.</p>
     </footer>
   )
 }
